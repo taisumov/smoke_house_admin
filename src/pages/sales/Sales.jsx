f58f3bc872dd641651b85a18751caefe6cf130ea
taisumov <islam.taisumov10@gmail.com>
2023-03-10 01:30:20 +0300

    Commit

--- src/pages/sales/Sales.jsx
+++ src/pages/sales/Sales.jsx
@@ -12,6 +12,7 @@ import MCheckbox from "../../components/UI/checkbox/MCheckbox";
 const Sales = () => {
   const [sales, setSales] = useState([""]);
   const [visible, setVisible] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
 
   function postData() {
     host
@@ -26,15 +27,18 @@ const Sales = () => {
   }
 
   function getData() {
+    setIsLoading(true);
     host
       .get("/api/promo")
       .then((res) => {
         console.log(res);
-        setSales(res["data"]);
+        setSales(res["data"]["sales"]);
+        setVisible(res["data"]["visible"]);
       })
       .catch((err) => {
         console.log(err, "get");
-      });
+      })
+      .finally(() => setIsLoading(false));
   }
 
   useEffect(() => {
@@ -42,59 +46,65 @@ const Sales = () => {
   }, []);
 
   return (
-    <div className={style.container}>
-      <div className={style.sales}>
-        {sales.map((sale, index) => (
-          <div className={style.saleContainer}>
-            <div className={style.controlBlock}>
-              <div className={style.title}>
-                <Mtitle>Акция {index + 1}</Mtitle>
-              </div>
-              <div
-                className={style.closeButton}
-                onClick={() => {
-                  setSales(sales.filter((sale, ind) => ind != index));
-                }}
-              >
-                ╳
-              </div>
+    <div>
+        {isLoading ?
+            <div className={style.container}>
+                <div className={style.sales}>
+                    {sales.map((sale, index) => (
+                        <div className={style.saleContainer}>
+                            <div className={style.controlBlock}>
+                                <div className={style.title}>
+                                    <Mtitle>Акция {index + 1}</Mtitle>
+                                </div>
+                                <div
+                                    className={style.closeButton}
+                                    onClick={() => {
+                                        setSales(sales.filter((sale, ind) => ind != index));
+                                    }}
+                                >
+                                    ╳
+                                </div>
+                            </div>
+
+                            <PostEditor
+                                text={sales[index]}
+                                setText={(value) => {
+                                    setSales(sales.map((el, ind) => (ind === index ? value : el)));
+                                }}
+                                sales={sales}
+                            />
+                        </div>
+                    ))}
+                </div>
+
+                <div className={style.addButton}>
+                    <MButton
+                        onClick={() => {
+                            setSales([...sales, ""]);
+                        }}
+                    >
+                        Добавить акцию
+                    </MButton>
+                </div>
+                <div className={style.uploadButton}>
+                    <div className={style.checkboxContainer}>
+                        <span className={style.checkboxContainer__text}>Отображать блок</span>
+                        <MCheckbox interview={visible} setInterview={setVisible}></MCheckbox>
+                    </div>
+                    <MButton
+                        onClick={(e) => {
+                            e.preventDefault();
+                            console.log(sales);
+                            postData();
+                        }}
+                    >
+                        Сохранить
+                    </MButton>
+                </div>
             </div>
-
-            <PostEditor
-              text={sales[index]}
-              setText={(value) => {
-                setSales(sales.map((el, ind) => (ind === index ? value : el)));
-              }}
-              sales={sales}
-            />
-          </div>
-        ))}
-      </div>
-
-      <div className={style.addButton}>
-        <MButton
-          onClick={() => {
-            setSales([...sales, ""]);
-          }}
-        >
-          Добавить акцию
-        </MButton>
-      </div>
-      <div className={style.uploadButton}>
-        <div className={style.checkboxContainer}>
-          <span className={style.checkboxContainer__text}>Отображать блок</span>
-          <MCheckbox interview={visible} setInterview={setVisible}></MCheckbox>
-        </div>
-        <MButton
-          onClick={(e) => {
-            e.preventDefault();
-            console.log(sales);
-            postData();
-          }}
-        >
-          Сохранить
-        </MButton>
-      </div>
+            :
+            <></>
+        }
     </div>
   );
 };
